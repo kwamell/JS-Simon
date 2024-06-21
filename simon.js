0250d227464e317ba5fb7d8139e21ed4545a3aaa
Kwamell Howard <107659595+kwamell@users.noreply.github.com>
2024-06-21 15:02:26 -0400

    Update Game Text

--- simon.js
+++ simon.js
@@ -66,7 +66,7 @@ function checkAnswer(currentLevel) {
         setTimeout(function () {
             $("body").removeClass("game-over");
         }, 200);
-        $("#level-title").text("Game Over, Press Any Key to Restart");
+        $("#level-title").text("Game Over! You ended at " + level + ". Press Any Key to Restart");
 
         setTimeout(function () {
             location.reload(true);
